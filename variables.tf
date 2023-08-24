d80e1d3f028f14fb756ca70af1f74b69f1299dec
armdupre <a-joseph.dupre@keysight.com>
2023-08-24 05:03:44 -0400

    Initial Checkin.

---
+++ variables.tf
@@ -0,0 +1,126 @@
+variable "AdminPassword" {
+	description = "Password associated with the VM administrator account"
+	sensitive = true
+	type = string
+}
+
+variable "AdminUserName" {
+	default = "azure"
+	description = "Id of the VM administrator account"
+	type = string
+}
+
+variable "EnableAcceleratedNetworking" {
+	default = false
+	description = "Enables SR-IOV on supported VMs to improve networking performance"
+	type = bool
+}
+
+variable "EnableIpForwarding" {
+	default = false
+	description = "Enables forwarding of network traffic to an address not assigned to VM"
+	type = bool
+}
+
+variable "Eth0SubnetId" {
+	description = "Id of the subnet associated with the first network interface"
+	type = string
+}
+
+variable "ImageSku" {
+	default = "keysight-ixload-virtual-edition-client"
+	description = "An instance of an offer, such as a major release of a distribution."
+	type = string
+}
+
+variable "ImageVersion" {
+	default = "9.36.0"
+	description = "The version number of an image SKU."
+	type = string
+}
+
+variable "InstanceId" {
+	default = "app"
+	description = "Id of the instance of this module that ensures uniqueness"
+	type = string
+}
+
+variable "MarketplaceImageOfferId" {
+	default = "keysight-ixload-virtual-edition"
+	description = "The name of a group of related images created by a publisher."
+	type = string
+}
+
+variable "MarketplaceImagePublisherId" {
+	default = "keysight-technologies-ixvm"
+	description = "The organization that created the image."
+	type = string
+}
+
+variable "ResourceGroupLocation" {
+	default = "East US"
+	description = "Location of container metadata and control plane operations"
+	type = string
+}
+
+variable "ResourceGroupName" {
+	description = "Id of container that holds related resources that you want to manage together"
+	type = string
+}
+
+variable "SshKeyName" {
+	description = "Id associated with SSH key pair used for authentication"
+	type = string
+}
+
+variable "Tag" {
+	default = "ixload"
+	description = "App ID tag of application using the deployment"
+	type = string
+}
+
+variable "UserEmailTag" {
+	default = "terraform@example.com"
+	description = "Email address tag of user creating the deployment"
+	type = string
+	validation {
+		condition = length(var.UserEmailTag) >= 14
+		error_message = "UserEmailTag minimum length must be >= 14."
+	}
+}
+
+variable "UserLoginTag" {
+	default = "terraform"
+	description = "Login ID tag of user creating the deployment"
+	type = string
+	validation {
+		condition = length(var.UserLoginTag) >= 4
+		error_message = "UserLoginTag minimum length must be >= 4."
+	}
+}
+
+variable "UserProjectTag" {
+	default = "module"
+	description = "Project tag of user creating the deployment"
+	type = string
+}
+
+variable "Version" {
+	default = "9-36"
+	description = "Versioning of the application using the deployment"
+	type = string
+}
+
+variable "VmSize" {
+	default = "Standard_F4s_v2"
+	description = "Category, series and instance specifications associated with the VM"
+	type = string
+	validation {
+		condition = contains([	"Standard_F4s_v2",	"Standard_F8s_v2"
+							], var.VmSize)
+		error_message = <<EOF
+VmSize must be one of the following sizes:
+	Standard_F4s_v2, Standard_F8s_v2
+		EOF
+	}
+}
